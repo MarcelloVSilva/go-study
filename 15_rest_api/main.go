7bb023b0b5effd890808843af5d8944611b821ec
Marcello Silva <marcellovcs@gmail.com>
2019-08-29 21:09:46 -0300

    new study: rest api with mux

---
+++ 15_rest_api/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+	"github.com/gorilla/mux"
+)
+
+type Article struct {
+	Title string
+	Desc string
+	Content string
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request){
+
+	articles := Articles{
+		Article{"Test Title", "Test Description", "Hello World"},
+	}
+	fmt.Println("Endpoint Hit: All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func testPostArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Test POST endpoint worked")
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage Endpoint Hit")
+}
+
+func handleRequest() {
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
+	myRouter.HandleFunc("/articles", testPostArticles).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8001", myRouter))
+}
+
+func main() {
+	handleRequest()
+}
